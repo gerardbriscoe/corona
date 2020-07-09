e3fff40b3319fcea5ca6cf1b6716a06be912938f
G <g@g-io.no-ip.org>
2020-07-09 23:14:09 +0100

    'gui update: dropdown ordering update'

--- vue-definitions.js
+++ vue-definitions.js
@@ -567,7 +567,7 @@ window.app = new Vue({
 		'-------------',
 		'North America','South America','Latin America','Europe','EU','Middle East','Africa','Asia','Oceania',
 		'-------------',
-		'USA (3206876)','Brazil (1755779)','India (794855)','Russia (707301)','Peru (312911)','Chile (306216)','Spain (300136)','UK (287621)','Mexico (275003)','Italy (242363)','South Africa (238339)','Germany (199162)','France (170094)','Colombia (128638)','Canada (106742)','Argentina (87030)','China (83581)','Belgium (62210)','Netherlands (50798)','Panama (41251)','Poland (36951)','Switzerland (32586)','Ireland (25565)','Austria (18615)','Czechia (12859)','Australia (10714)','Estonia (2011)','Slovenia (1776)','New Zealand (1540)',
+		'USA (3213283)','Brazil (1755779)','India (794855)','Russia (707301)','Peru (316448)','Chile (306216)','Spain (300136)','UK (287621)','Mexico (275003)','Italy (242363)','South Africa (238339)','Germany (199198)','France (170094)','Colombia (133973)','Canada (106742)','Argentina (87030)','China (83581)','Belgium (62210)','Netherlands (50798)','Panama (41251)','Poland (36951)','Switzerland (32586)','Ireland (25565)','Austria (18615)','Czechia (12859)','Australia (10714)','Estonia (2011)','Slovenia (1776)','New Zealand (1540)',
 		'-------------',
 		'England','Scotland','Wales',
 		'-------------',
